0069c8ae0c8c05e39830ceac5f5d71275db7a1f5
George-H12 <gee9195@nyu.edu>
2023-12-03 01:30:36 -0500

    Create models

---
+++ api/models/post.model.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+
+const postArt = new mongoose.Schema(
+    {
+        image: {
+            type: String,
+            required: true,
+          },
+          description: {
+            type: String,
+            max: 50,
+            required: true,
+          },
+          likeButton: {
+            type: Number,
+            default: 0,
+          },
+          price: {
+            type: Number,
+            required: true,
+          },
+          forSale: {
+            type: Boolean,
+            default: true,
+          },
+          timestamp: {
+            type: Date,
+            default: Date.now,
+          },
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+          },
+    },
+);
+
+const Post = mongoose.model('Post', postArt);
+
+export default Post;
